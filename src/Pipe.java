f9dd0005d0545039fe756cb12ea97234e89306b2
Martin <martin.stoecker99@gmail.com>
2019-01-07 14:55:42 +0100

    add some changes

--- src/Pipe.java
+++ src/Pipe.java
@@ -1,2 +1,16 @@
 public class Pipe {
+    private boolean isopen;
+
+    public Pipe(){
+        this.isopen = isopen;
+    }
+
+    public void closePipe(){
+        isopen = false;
+    }
+
+    public void openPipe(){
+        isopen = true;
+    }
+    
 }
