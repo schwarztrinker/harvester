0cfc8210683d050191d2cff2a96c1f1cfe0b29d0
Martin <martin.stoecker99@gmail.com>
2019-01-07 15:16:08 +0100

    changed harvester to main

--- src/Harvester.java
+++ src/Harvester.java
@@ -1,10 +1,10 @@
 public class Harvester {
 
-    public Harvester(){
-        Engine engine = new Engine(560);
-        CuttingUnit cuttingUnit = new CuttingUnit(9);
-        Pipe pipe = new Pipe(6, false);
-        Storage storage = new Storage(12000);
+    public Harvester(int horsePower, int cuttingUnitWidth, int pipeLenght, boolean pipeOpen, int maxStorage){
+        Engine engine = new Engine( horsePower);
+        CuttingUnit cuttingUnit = new CuttingUnit(cuttingUnitWidth);
+        Pipe pipe = new Pipe(pipeLenght, pipeOpen);
+        Storage storage = new Storage(maxStorage);
         Wheel wheel[] = new Wheel[4];
         for (int i= 0; i <  4; i++ ){
             if(i < 2) wheel[i] = new Wheel(3.2, true);
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //New Harvester
+        Harvester harvester = new Harvester(650,  9, 10, false, 6500);
+
     }
 }
